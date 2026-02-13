0fa7652ce9fbae4a74e3ba6c5ca00f8d2ef3436a
Chala785 <juan_gallego82241@elpoli.edu.co>
2026-02-12 20:13:55 -0500

    Lista ligadas

---
+++ factorial.py
@@ -0,0 +1,13 @@
+# def factorial (n):
+#     if n <= 1:
+#         return 1
+#     return n * factorial(n - 1)
+
+# print(factorial(5))
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(20))
