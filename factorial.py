c3b59c291f7d26b774c20e5aeba79a0c3cf5d7ed
Chala785 <juan_gallego82241@elpoli.edu.co>
2026-02-19 20:00:57 -0500

    Factorial

--- factorial.py
+++ factorial.py
@@ -7,9 +7,10 @@
 
 # -------------- Fibonacci --------------
 
-def fibonacci(n):                                                                                                      
-    if n <= 1:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+# def fibonacci(n):                                                                                                      
+#     if n <= 1:
+#         return n
+#     return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(fibonacci(20))
+# print(fibonacci(20))
+
